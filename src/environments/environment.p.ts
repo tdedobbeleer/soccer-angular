5788a1733d67d4f6ae80532d082c706ece346a85
u0090265 <tom.dedobbeleer@kuleuven.be>
2017-07-19 09:29:12 +0200

    added quality as env

---
+++ src/environments/environment.p.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    api_url: "https://papi-svkvoetbal.rhcloud.com"
+};
--- src/environments/environment.q.ts
+++ src/environments/environment.q.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
+    production: false,
     api_url: "https://qapi-svkvoetbal.rhcloud.com"
 };
