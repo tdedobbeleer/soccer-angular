7ff7c6131e6748d15248f3848c506b322546a997
u0090265 <tom.dedobbeleer@kuleuven.be>
2017-08-27 16:47:43 +0200

    final api url, I hope

--- src/environments/environment.p.ts
+++ src/environments/environment.p.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    api_url: "https://papi-svkvoetbal.rhcloud.com",
+    api_url: "https://svk-p-01.herokuapp.com",
     recaptcha_public_key: "dummy"
 };
