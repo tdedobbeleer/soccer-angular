cea0d56d64b2007b6530c95f8636e797da7ed68c
tdedobbeleer <tdedobbeleer@gmail.com>
2020-08-24 08:37:10 +0200

    Update environment.q.ts

--- src/environments/environment.q.ts
+++ src/environments/environment.q.ts
@@ -5,5 +5,5 @@ export const environment = {
     weather_api_key: "ed947b1f42105d993e46d24c3d770be2",
     weather_api_country: "be",
     weather_api_url: "https://api.openweathermap.org",
-    recaptcha_public_key: "6LfrWikTAAAAALTtOY78xlgzSQM46UogVdn6f03y"
+    recaptcha_public_key: "6LcxicIZAAAAALT9hsIKOe2x8tYu5fX5YLxcVkmg"
 };
