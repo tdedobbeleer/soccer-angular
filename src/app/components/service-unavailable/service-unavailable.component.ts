ab3988d202e88921206c0c5f6ecbf16ebd50506e
Tom <tdedobbeleer@gmail.com>
2020-09-05 09:40:33 +0200

    better service down handling

--- src/app/components/service-unavailable/service-unavailable.component.ts
+++ src/app/components/service-unavailable/service-unavailable.component.ts
@@ -1,19 +1,25 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-service-unavailable',
     template: `
-  <div class="container">
-    <div class="row m-t-1">
-        <div class="col-md-12">
-            <h2>{{'title.service.unavailable' | translate}}</h2>
-            <div class="alert alert-warning">
-                {{'text.service.unavailable' | translate}}
+        <div class="container">
+            <div class="row m-t-1 m-b-1">
+                <div class="col-md-12">
+                    <h2>{{'title.service.unavailable' | translate}}</h2>
+                    <div class="alert alert-warning">
+                        {{'text.service.unavailable' | translate}}
+                    </div>
+                </div>
+                <div class="col-lg-4 col-lg-offset-4">
+                    <div class="text-center">
+                        <img src="assets/images/pexels-thisisengineering-3861958.jpg" class="thumbnail img-responsive rounded"
+                             alt="service down">
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  </div> 
-  `,
+    `,
     styles: []
 })
 export class ServiceUnavailableComponent implements OnInit {
--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from "@angular/core";
-import {Router} from "@angular/router";
-import {ErrorUtil} from "../classes/error-util";
-import {TranslationService} from "./translation.service";
-import {HttpErrorResponse} from "@angular/common/http";
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {ErrorUtil} from '../classes/error-util';
+import {TranslationService} from './translation.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Injectable()
 export class ErrorHandlerService {
@@ -10,7 +10,7 @@ export class ErrorHandlerService {
     constructor(private _router: Router, private _translationService: TranslationService) {
     }
 
-    handle(error: HttpErrorResponse | any, url = "/") {
+    handle(error: HttpErrorResponse | any, url = '/') {
         if (error instanceof HttpErrorResponse) {
             switch (error.status) {
                 case 401:
@@ -22,6 +22,7 @@ export class ErrorHandlerService {
                 case 400:
                     let d = error.error;
                     return ErrorUtil.getValidationError(d, this._translationService.currentLang());
+                case 0: //cors error
                 case 503:
                     this._router.navigate(['/unavailable']);
                     break;
