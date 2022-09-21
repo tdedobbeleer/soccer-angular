df594b5a08b7dbfabf0a33ba7c38e96eb91b8632
tdedobbeleer <tdedobbeleer@gmail.com>
2022-09-21 18:26:41 +0200

    revert to heroku

--- src/environments/environment.p.ts
+++ src/environments/environment.p.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     defaultTeamName: 'SVK',
-    api_url: 'https://soccer-ws-4gk3xr7fuq-ew.a.run.app',
+    api_url: 'https://svk-p-01.herokuapp.com',
     weather_api_key: 'ed947b1f42105d993e46d24c3d770be2',
     weather_api_country: 'be',
     weather_api_url: 'https://api.openweathermap.org',
