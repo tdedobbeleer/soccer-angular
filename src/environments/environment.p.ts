e23752bf59f11387ff8846286402bdb7c9f1d179
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-20 17:07:50 +0100

    move to standard SSL port

--- src/environments/environment.p.ts
+++ src/environments/environment.p.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     defaultTeamName: 'SVK',
-    api_url: 'https://svk-api.ddns.net:8443',
+    api_url: 'https://svk-api.ddns.net',
     weather_api_key: 'ed947b1f42105d993e46d24c3d770be2',
     weather_api_country: 'be',
     weather_api_url: 'https://api.openweathermap.org',
