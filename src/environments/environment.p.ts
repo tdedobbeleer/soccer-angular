6660adbf22d092dc14de988728bf6b21fe5604bd
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-29 19:56:16 +0100

    change api url

--- src/environments/environment.p.ts
+++ src/environments/environment.p.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     defaultTeamName: 'SVK',
-    api_url: 'https://svk-api.ddns.net',
+    api_url: 'https://api-p-1.svk.freeddns.org',
     weather_api_key: 'ed947b1f42105d993e46d24c3d770be2',
     weather_api_country: 'be',
     weather_api_url: 'https://api.openweathermap.org',
